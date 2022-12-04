be6ec947ac354d4ff84bec75e9d5cca355a45af7
Ioan Bizău <i@ibz.me>
2022-12-04 15:23:29 +0200

    Go with Tide.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
 use std::{
     collections::HashMap,
     env,
-    fmt,
     path::{Path, PathBuf},
     fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
+    sync::{Arc, RwLock},
 };
 use chrono::NaiveDate;
-use markdown::{CompileOptions, Options, to_html_with_options};
-use mime;
-use mime_guess;
+use http_types::mime;
+use markdown;
 use serde::{Serialize, Deserialize};
-use tera::Context;
-use tera::Tera;
+use tera;
+use tide::{Request, Response};
 use toml;
 use yaml_front_matter::{Document, YamlFrontMatter};
 
@@ -24,6 +21,7 @@ struct SiteConfig {
     site: Site,
 }
 
+#[derive(Clone)]
 #[derive(Serialize)]
 #[derive(Deserialize)]
 struct Site {
@@ -41,6 +39,13 @@ struct PageMetadata {
 }
 
 #[derive(Serialize)]
+struct Page {
+    title: String,
+    path: String,
+    filename: String,
+}
+
+#[derive(Serialize)]
 struct Post {
     title: String,
     slug: String,
@@ -48,77 +53,114 @@ struct Post {
     filename: String,
 }
 
-enum HttpStatus {
-    Http200,
-    Http400,
-    Http404,
-    Http405,
+#[derive(Clone)]
+struct SiteState {
+    path: PathBuf,
+    site: Site,
+    posts: Arc<RwLock<HashMap<String, Post>>>,
+    pages: Arc<RwLock<HashMap<String, Page>>>,
+    tera: tera::Tera,
 }
 
-impl fmt::Display for HttpStatus {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            HttpStatus::Http200 => write!(f, "HTTP/1.1 200 OK"),
-            HttpStatus::Http400 => write!(f, "HTTP/1.1 400 Bad Request"),
-            HttpStatus::Http404 => write!(f, "HTTP/1.1 404 Not Found"),
-            HttpStatus::Http405 => write!(f, "HTTP/1.1 405 Method Not Allowed"),
-	}
-    }
+#[derive(Clone)]
+struct State {
+    sites: HashMap<String, SiteState>,
 }
 
-fn main() {
+#[async_std::main]
+async fn main() -> Result<(), std::io::Error> {
+    femme::start();
+
+    let mut app = tide::with_state(State { sites: get_sites() });
+    app.with(tide::log::LogMiddleware::new());
+
+    app.at("/posts/:slug").get(|request: Request<State>| async move {
+	let site = &request.state().sites["default"];
+        let slug = request.param("slug")?;
+	let response = match site.posts.read().unwrap().get(&slug as &str) {
+	    Some(post) => {
+		let body = render_markdown(&site, &post.filename);
+		Response::builder(200).content_type(mime::HTML).body(body).build()
+	    },
+	    None => {
+		Response::new(404)
+	    }
+	};
+	Ok(response)
+    });
+    app.at("/").get(|request: Request<State>| async move {
+	let site = &request.state().sites["default"];
+	let pages = site.pages.read().unwrap();
+	let index = pages.get("index").unwrap();
+	let body = render_markdown(&site, &index.filename);
+	let response = Response::builder(200).content_type(mime::HTML).body(body).build();
+	Ok(response)
+    });
+    app.at("/*path").get(|request: Request<State>| async move {
+	let site = &request.state().sites["default"];
+	let path = request.param("path")?;
+	let response = match site.pages.read().unwrap().get(&path as &str) {
+	    Some(page) => {
+		let body = render_markdown(&site, &page.filename);
+		Response::builder(200).content_type(mime::HTML).body(body).build()
+	    },
+	    None => {
+		let mut file_path = PathBuf::from(&site.path);
+		file_path.push(path);
+
+		let content = match fs::read(&file_path) {
+		    Ok(content) => content,
+		    _ => {
+			return Ok(Response::new(404));
+		    }
+		};
+
+		let content_type = match mime::Mime::sniff(&content) {
+		    Ok(m) => m,
+		    _ => {
+			mime::Mime::from_extension(&file_path.extension().unwrap().to_str().unwrap()).unwrap()
+		    }
+		};
+
+		Response::builder(200).content_type(content_type).body(content).build()
+	    }
+	};
+	Ok(response)
+    });
+
+    app.listen(BIND_ADDR).await?;
+
+    Ok(())
+}
+
+fn get_sites() -> HashMap<String, SiteState> {
+    // NB: only load the "default" site for now
+
     let mut site_path = PathBuf::from(env::current_dir().unwrap());
     site_path.push("sites");
     site_path.push("default");
 
-    let posts = get_posts(&site_path);
+    let mut site_config_path = PathBuf::from(&site_path);
+    site_config_path.push("config.toml");
+    let site_config: SiteConfig = toml::from_str(&fs::read_to_string(&site_config_path).unwrap()).unwrap();
 
-    let mut tera = Tera::new(&format!("{}/templates/*.html", site_path.to_str().unwrap())).unwrap();
+    let posts = get_posts(&site_path);
+    let pages = get_pages(&site_path);
+
+    let mut tera = tera::Tera::new(&format!("{}/templates/*.html", site_path.to_str().unwrap())).unwrap();
     tera.autoescape_on(vec![]);
 
-    let listener = TcpListener::bind(BIND_ADDR).unwrap();
+    let mut sites = HashMap::new();
+    sites.insert("default".to_string(),
+		 SiteState {
+		     path: site_path,
+		     site: site_config.site,
+		     posts: Arc::new(RwLock::new(posts)),
+		     pages: Arc::new(RwLock::new(pages)),
+		     tera: tera,
+		 });
 
-    for stream in listener.incoming() {
-	handle_connection(stream.unwrap(), &site_path, &posts, &tera);
-    }
-}
-
-fn handle_connection(mut stream: TcpStream, site_path: &PathBuf, posts: &HashMap<String, Post>, tera: &Tera) {
-    let buf_reader = BufReader::new(&mut stream);
-    let mut lines = buf_reader.lines();
-    let request_line = match lines.next() {
-	Some(l) => l,
-	None => {
-	    return;
-	}
-    }.unwrap();
-
-    let (method, request_path) = match request_line.split(" ").collect::<Vec<_>>()[..] {
-	[method, path, "HTTP/1.1"] => (method, path),
-	_ => {
-	    send_response(stream, HttpStatus::Http400, mime::TEXT_HTML_UTF_8, &[].to_vec());
-	    return;
-	}
-    };
-
-    match method {
-	"GET" => {
-	    let (status, mime, content) = handle_get(&site_path, &request_path, &posts, &tera);
-	    send_response(stream, status, mime, &content);
-	    return;
-	},
-	_ => {
-	    send_response(stream, HttpStatus::Http405, mime::TEXT_HTML_UTF_8, &[].to_vec());
-	    return;
-	},
-    };
-}
-
-fn send_response(mut stream: TcpStream, status: HttpStatus, mime: mime::Mime, content: &Vec<u8>) {
-    let length = content.len();
-    stream.write_all(format!("{status}\r\nContent-Length: {length}\r\n").as_bytes()).unwrap();
-    stream.write_all(format!("Content-Type: {mime}\r\n\r\n").as_bytes()).unwrap();
-    stream.write_all(content).unwrap();
+    sites
 }
 
 fn get_post(path: &PathBuf) -> Option<Post> {
@@ -153,13 +195,13 @@ fn get_post(path: &PathBuf) -> Option<Post> {
 	}
     };
 
-    return Some(
+    Some(
 	Post {
 	    title: String::from(&document.metadata.title),
 	    slug: Path::new(&filename[11..]).file_stem().unwrap().to_str().unwrap().to_string(),
 	    date: date,
 	    filename: Path::new(&path).display().to_string(),
-    });
+    })
 }
 
 fn get_posts(site_path: &PathBuf) -> HashMap<String, Post> {
@@ -177,153 +219,73 @@ fn get_posts(site_path: &PathBuf) -> HashMap<String, Post> {
 	    }
 	};
     }
-    return posts;
+
+    posts
 }
 
-fn handle_get(site_path: &PathBuf, request_path: &str, posts: &HashMap<String, Post>, tera: &Tera) -> (HttpStatus, mime::Mime, Vec<u8>) {
-    let status: HttpStatus;
-    let mime: mime::Mime;
-    let content: Vec<u8>;
-    match path_from_request_path(&site_path, request_path, &posts) {
-	Ok(path) => {
-	    match render_path(&site_path, &path, &posts, &tera) {
-		(Some(m), c) => (status, mime, content) = (HttpStatus::Http200, m, c),
-		(None, _) => {
-		    status = HttpStatus::Http404;
-		    (mime, content) = render_404(&site_path, &tera);
-		}
-	    }
-	},
-	Err(HttpStatus::Http404) => {
-	    status = HttpStatus::Http404;
-	    (mime, content) = render_404(&site_path, &tera);
-	}
-	Err(s) => {
-	    (status, mime, content) = (s, mime::TEXT_HTML_UTF_8, [].to_vec());
-	},
-    }
-    return (status, mime, content);
-}
-
-fn path_from_request_path(site_path: &PathBuf, request_path: &str, posts: &HashMap<String, Post>) -> Result<PathBuf, HttpStatus> {
-    if !request_path.starts_with('/') {
-	return Err(HttpStatus::Http400);
-    }
-
-    let mut parts: Vec<&str> = match request_path {
-	"/" => vec!["index"],
-	_ => request_path[1..].split("/").collect::<Vec<_>>(),
-    };
-
-    match (parts.first().unwrap(), parts.len()) {
-	(&"posts", 1) => {
-	    parts.insert(0, "pages"); /* /posts/ is a "page" -> /pages/posts.md */
-	},
-	(&"posts", _) => {
-	    match posts.get(parts[1]) {
-		Some(post) => {
-		    return Ok(PathBuf::from(&post.filename));
-		},
-		None => {
-		    return Err(HttpStatus::Http404);
-		}
-	    };
-	},
-	(&"static", _) => {},
+fn get_page(path: &PathBuf) -> Option<Page> {
+    let filename = path.file_name().unwrap().to_str().unwrap();
+    let content = match fs::read_to_string(&path.display().to_string()) {
+	Ok(content) => content,
 	_ => {
-	    /* everything that is not /static/... or /posts/... is a "page" and should be looked up under pages/ */
-	    parts.insert(0, "pages");
-	},
+	    println!("Cannot read from: {}. Skipping!", path.display());
+	    return None;
+	}
     };
 
-    let mut path = PathBuf::from(site_path);
-    for (i, part) in parts.iter().enumerate() {
-	if part.starts_with('.') {
-	    return Err(HttpStatus::Http400);
+    let document = match YamlFrontMatter::parse::<PageMetadata>(&content) {
+	Ok(document) => document,
+	_ => {
+	    println!("Invalid post: {}. Skipping!", path.display());
+	    return None;
 	}
-	if i < parts.len() - 1 && !path.exists() {
-	    return Err(HttpStatus::Http404);
-	}
-	let filename = if i < parts.len() - 1 || !part.contains('?') {
-	    part.to_string()
-	} else {
-	    part[..part.find('?').unwrap()].to_string()
+    };
+
+    return Some(
+	Page {
+	    title: String::from(&document.metadata.title),
+	    path: Path::new(&filename).file_stem().unwrap().to_str().unwrap().to_string(),
+	    filename: Path::new(&path).display().to_string(),
+    });
+}
+
+fn get_pages(site_path: &PathBuf) -> HashMap<String, Page> {
+    let mut pages = HashMap::new();
+    let mut path = PathBuf::from(&site_path);
+    path.push("pages");
+
+    for p in fs::read_dir(&path).unwrap() {
+	match get_page(&p.unwrap().path()) {
+	    Some(page) => {
+		pages.insert(page.path.to_string(), page);
+	    }
+	    None => {
+		continue;
+	    }
 	};
-	path.push(filename);
     }
 
-    match path.extension() {
-	None => {
-	    for ext in &["md", "html"] {
-		path.set_extension(ext);
-		if path.exists() {
-		    return Ok(path);
-		}
-	    }
-	},
-	_ => {
-	    if path.exists() {
-		return Ok(path);
-	    }
-	},
-    }
-
-    return Err(HttpStatus::Http404);
+    pages
 }
 
-fn render_404(site_path: &PathBuf, tera: &Tera) -> (mime::Mime, Vec<u8>) {
-    match render_path(&site_path, &PathBuf::from("pages/404.md"), &HashMap::new(), &tera) {
-	(Some(m), c) => {
-	    return (m, c);
-	},
-	_ => {
-	    return (mime::TEXT_HTML_UTF_8, "<html><body><blink>404 page not found</blink></body></html>".as_bytes().to_vec());
-	}
-    }
-}
-
-fn render_path(site_path: &PathBuf, path: &PathBuf, posts: &HashMap<String, Post>, tera: &Tera) -> (Option<mime::Mime>, Vec<u8>) {
-    match path.extension() {
-	None => (None, [].to_vec()),
-	Some(os_str) => {
-	    match os_str.to_str() {
-		Some("html") => (Some(mime::TEXT_HTML_UTF_8), render_html(site_path, path)),
-		Some("md") => (Some(mime::TEXT_HTML_UTF_8), render_markdown(site_path, path, &posts, &tera)),
-		_ => render_static(site_path, path),
-	    }
-	}
-    }
-}
-
-fn render_html(site_path: &PathBuf, path: &PathBuf) -> Vec<u8> {
-    return fs::read_to_string([site_path, path].iter().collect::<PathBuf>()).unwrap().as_bytes().to_vec();
-}
-
-fn render_markdown(site_path: &PathBuf, path: &PathBuf, posts: &HashMap<String, Post>, tera: &Tera) -> Vec<u8> {
-    let md = fs::read_to_string([site_path, path].iter().collect::<PathBuf>()).unwrap();
+fn render_markdown(site_state: &SiteState, path: &str) -> Vec<u8> {
+    let md = fs::read_to_string([&site_state.path, &PathBuf::from(path)].iter().collect::<PathBuf>()).unwrap();
     let document: Document<PageMetadata> = YamlFrontMatter::parse::<PageMetadata>(&md).unwrap();
-    let mut context = Context::new();
-    let mut site_config_path = PathBuf::from(site_path);
-    site_config_path.push("config.toml");
-    let site_config_content = fs::read_to_string(&site_config_path).unwrap();
-    let site_config: SiteConfig = toml::from_str(&site_config_content).unwrap();
-    context.insert("site", &site_config.site);
+
+    let mut context = tera::Context::new();
+    context.insert("site", &site_state.site);
     context.insert("page", &document.metadata);
-    let mut posts_list: Vec<&Post> = posts.into_iter().map(|(_k, p)| p).collect();
+    let posts = site_state.posts.read().unwrap();
+    let mut posts_list: Vec<&Post> = posts.values().into_iter().collect();
     posts_list.sort_by(|a, b| b.date.cmp(&a.date));
     context.insert("posts", &posts_list);
-    let rendered_content = Tera::one_off(&document.content, &context, true).unwrap();
-    let options = &Options {compile: CompileOptions {allow_dangerous_html: true,
-						     ..CompileOptions::default()},
-			    ..Options::default()};
-    let html_content = &to_html_with_options(&rendered_content, &options).unwrap();
-    context.insert("content", &html_content);
-    return tera.render("page.html", &context).unwrap().as_bytes().to_vec();
-}
 
-fn render_static(site_path: &PathBuf, path: &PathBuf) -> (Option<mime::Mime>, Vec<u8>) {
-    let mut file = fs::File::open([site_path, path].iter().collect::<PathBuf>()).unwrap();
-    let mut content: Vec<u8> = Vec::with_capacity(file.metadata().unwrap().len() as usize);
-    file.read_to_end(&mut content).unwrap();
-    return (mime_guess::from_path(path).first(), content);
+    let rendered_content = tera::Tera::one_off(&document.content, &context, true).unwrap();
+    let options = &markdown::Options {compile: markdown::CompileOptions {allow_dangerous_html: true,
+									 ..markdown::CompileOptions::default()},
+				      ..markdown::Options::default()};
+    let html_content = &markdown::to_html_with_options(&rendered_content, &options).unwrap();
+    context.insert("content", &html_content);
+
+    site_state.tera.render("page.html", &context).unwrap().as_bytes().to_vec()
 }
